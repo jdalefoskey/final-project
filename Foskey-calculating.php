e3178694a2fd06700829d398ec041dcac078a880
Dale Foskey <developer.dale.foskey@gmail.com>
2021-04-27 21:08:24 -0400

    Part 1 of Final Project
    created the html file, created associated php files

---
+++ Foskey-calculating.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Scores and Arrays</title>
+</head>
+<body>
+    <h1>Arrays with functions</h1>
+
+<?php
+
+    include("Foskey-inc-calc-function.php");
+
+?>
+    <p><a href="Foskey-calculating.html">Return to beginning</a></p>
+</body>
+</html>
